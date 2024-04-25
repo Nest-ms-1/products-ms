ef8deaf8a5cb539e4ba52c1c4b5eed3e47dc5640
Jhonatan Estiven Becerra Pedrozo <jsbpdeveloper@gmail.com>
2024-04-24 20:41:01 -0500

    first commit

---
+++ src/config/envs.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import * as Joi from 'joi'
+
+interface EnvVars {
+  PORT: number
+  DATABASE_URL: string
+}
+
+const envsSchema = Joi.object({
+  PORT: Joi.number().required(),
+  DATABASE_URL: Joi.string().required(),
+}).unknown(true)
+
+const { error, value } = envsSchema.validate(process.env)
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
+const envsVars: EnvVars = value
+export const envs = {
+  port: envsVars.PORT,
+  databaseUrl: envsVars.DATABASE_URL,
+}
